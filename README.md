8c624026cb0656280ecff5322c471472dfa0b208
Alejandro Piad-Morffis <apiad@apiad.net>
2023-09-21 08:21:18 -0400

    Update

--- README.md
+++ README.md
@@ -27,3 +27,35 @@ Visitar <localhost:8501> para ver el demo.
 - El código en `app.py` puede servir para entender como usar los métodos en `sensorlib.py`.
 
 **NOTA**: La primera vez que se ejecuta el código se debe descargar un modelo de `transformers` de 2GB aproximadamente. Esto puede demorar.
+
+## Entrenando el modelo de NER
+
+Para entrenar el modelo de NER, primero hay que convertir los datos al formato de `spacy`:
+
+```
+python3 isabial_to_spacy.py
+```
+
+En la carpeta `Data`  aparecerán 3 archivos `*.spacy`.
+
+Luego hay dos opciones:
+
+### Modelo CPU (lento y malo)
+
+Es fácil, solo ejecutar:
+
+```
+python3 -m spacy train config_cpu.cfg --output ./Data
+```
+
+### Modelo GPU
+
+Necesitas instalar `cupy`. Esto es complicado, requiere CUDA y otras dependencias. Sigue las instrucciones [aquí](https://docs.cupy.dev/en/stable/install.html#).
+
+Una vez instalado y correctamente configurado `cupy`, ejecutar:
+
+```
+python3 -m spacy train config_gpu.cfg --gpu-id 0 --output ./Data
+```
+
+Donde `--gpu-id 0` apunta a la GPU a utilizar (0, 1, etc.)
--- app.py
+++ app.py
@@ -1,81 +1,28 @@
 import streamlit as st
-import spacy
+import json
 from transformers import pipeline
-from sensorlib import Annotation, taxonomy, top_k_terms, select_terms, convert_to_brat
 
 
 st.set_page_config(page_title="Sensors", layout="wide", page_icon="🧠")
 
 
-k_terms = st.sidebar.number_input("Candidate terms to extract", value=10)
-threshold = st.sidebar.slider("Classification threshold", value=0.5, min_value=0.0, max_value=1.0)
-
-@st.cache_resource
-def load_parser():
-    return spacy.load("en_core_web_lg")
-
-
-@st.cache_resource
-def load_classifier():
-    return pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+def load_jsonl(fname):
+    with open(fname) as fp:
+        return [json.loads(l) for l in fp]
 
 
 @st.cache_data
-def parse_document(_nlp, text):
-    return _nlp(text)
+def load_data():
+    train = load_jsonl("Data/anotacionesFinalTrain.jsonl")
+    eval = load_jsonl("Data/anotacionesFinalEval.jsonl")
+    test = load_jsonl("Data/anotacionesFinalTest.jsonl")
+
+    return train, eval, test
 
 
-@st.cache_resource
-def load_taxonomy(_nlp):
-    return taxonomy("ieee_taxonomy_v3.xlsx", _nlp)
+with st.spinner("Loading data..."):
+    train, eval, test = load_data()
 
 
-text = st.text_area("Input document", height=10)
-
-if not text:
-    st.warning("Please introduce the text...")
-    st.stop()
-
-nlp = load_parser()
-document = parse_document(nlp, text)
-terms = load_taxonomy(nlp)
-classifier = load_classifier()
-
-sentences = list(document.sents)
-
-cols = st.columns(4)
-cols[0].metric("Sentences", len(sentences))
-cols[1].metric("Terms in the Taxonomy", len(terms))
-
-
-@st.cache_data
-def extract_candidate_terms(sentence, count, _terms):
-    return top_k_terms(sentence, _terms, count)
-
-
-def score_terms(sentence, top_terms, _classifier):
-    return _classifier(sentence, candidate_labels=[str(annotation.term) for annotation in top_terms], multi_label=True)
-
-
-annotations = []
-
-
-for sentence in sentences:
-    st.info(sentence)
-
-    with st.spinner("Extracting terms"):
-        top_terms: list[Annotation] = top_k_terms(sentence, terms, k_terms)
-        scores = score_terms(sentence.text, top_terms, classifier)
-
-    for annotation, score in select_terms(top_terms, scores, threshold).items():
-        st.write(annotation.term.text, score)
-        annotation.score = score
-        annotations.append(annotation)
-
-    st.json(scores, expanded=False)
-
-st.write("### Annotations")
-
-st.table([annotation.to_dict() for annotation in annotations])
-
-st.code(convert_to_brat(annotations))
+with st.expander("Training"):
+    st.json(train)
